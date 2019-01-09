1b969a130efe21fc3973bcd534c019fca8754bd3
code5five <codefive65@gmail.com>
2019-01-09 10:54:42 +0700

    Clean up existing code

---
+++ head.php
@@ -0,0 +1,24 @@
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>PT Tiga Sinar Mandiri</title>
+
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
+  <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
+  <link rel="stylesheet" href="bower_components/morris.js/morris.css">
+  <link rel="stylesheet" href="bower_components/jvectormap/jquery-jvectormap.css">
+  <link rel="stylesheet" href="bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css">
+  <link rel="stylesheet" href="bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
+  <link rel="stylesheet" href="bower_components/bootstrap-daterangepicker/daterangepicker.css">
+  <link rel="stylesheet" href="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+
+  <!-- Google Font -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
--- header.php
+++ header.php
@@ -1,40 +1,33 @@
-<?php 
-
-    $user = $_SESSION['username'];
-    $sql = mysqli_query($conn, "SELECT * FROM tbl_user WHERE username='$user'");
-    $userdata = mysqli_fetch_array($sql);
-
-?>
 <a href="?page=dashboard" class="logo">
     <span class="logo-mini"><b>A</b>DM</span>
     <span class="logo-lg">
-    <i class="fa fa-user"></i> <?= $userdata['username'] ?>
+        <i class="fa fa-user"></i> Test
     </span>
 </a>
 <nav class="navbar navbar-static-top" role="navigation">
-    <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+  <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
     <span class="sr-only">Toggle navigation</span>
-    </a>
-    <!-- Navbar Right Menu -->
-    <div class="navbar-custom-menu">
-    <ul class="nav navbar-nav">
-        <li class="dropdown user user-menu">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-            <img src="img/pln.png" class="user-image" alt="User Image">
-                <span class="hidden-xs"><?= strtoupper($userdata['username']) ?></span>
-            </a>
-            <ul class="dropdown-menu">
-                <li class="user-header">
-                <img src="img/pln.png" class="img-circle" alt="User Image">
-                    <p>
-                        <?= "<b>".$userdata['username'] ."</b>"; ?>
-                    </p>
-                </li>
-                <li class="user-footer">
-                    <a href="?page=logout" class="btn btn-default btn-flat"><i class="fa fa-sign-out"></i> Sign out</a>
-                </li> 
-            </ul>
-        </li>
-    </ul>
-    </div>
+  </a>
+  <!-- Navbar Right Menu -->
+  <div class="navbar-custom-menu">
+      <ul class="nav navbar-nav">
+          <li class="dropdown user user-menu">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                  <img src="img/default_head.png" class="user-image" alt="User Image">
+                  <span class="hidden-xs">Test</span>
+              </a>
+              <ul class="dropdown-menu">
+                  <li class="user-header">
+                      <img src="img/default_head.png" class="img-circle" alt="User Image">
+                      <p>
+                        Test
+                      </p>
+                  </li>
+                  <li class="user-footer">
+                      <a href="?page=logout" class="btn btn-default btn-flat"><i class="fa fa-sign-out"></i> Sign out</a>
+                  </li>
+              </ul>
+          </li>
+      </ul>
+  </div>
 </nav>
