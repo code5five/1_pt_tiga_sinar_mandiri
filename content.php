df97ff082eeab675da6eb624571305512cd74742
code5five <codefive65@gmail.com>
2019-01-11 07:57:45 +0700

    Create settings user page

--- content.php
+++ content.php
@@ -4,6 +4,7 @@ if (isset($_GET['page'])) {
   $page = $_GET['page'];
 
   if ($page == 'dashboard') { }
+  elseif ($page == 'user') { include "page/user.php"; }
 
 } elseif (isset($_GET['action'])) {
 
---
+++ page/user.php
@@ -0,0 +1,44 @@
+<section class="content-header">
+  <h1>
+    User
+    <small>Manajemen pengguna</small>
+  </h1>
+  <ol class="breadcrumb">
+    <li><a href="#"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+    <li><a href="#">Settings</a></li>
+    <li class="active">User</li>
+  </ol>
+</section>
+
+<section class="content">
+
+  <div class="row">
+
+    <div class="col-md-12">
+      <div class="box box-success box-solid">
+        <div class="box-header with-border">
+          <h3 class="box-title">Data User</h3>
+
+          <div class="box-tools pull-right">
+            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+            </button>
+          </div>
+        </div>
+
+        <div class="box-body" style="">
+          <a href="" class="btn btn-md btn-success"><i></i> Tambah User</a>
+
+          <div class="row">
+            <div class="col-md-12">
+              <table class="tbl">
+                
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+</section>
