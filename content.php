45e91d24021f6919e5f6cbe8ac67d8ecc15ca88c
code5five <codefive65@gmail.com>
2019-01-16 07:42:40 +0700

    Refactor role content

--- content.php
+++ content.php
@@ -9,7 +9,7 @@ if (isset($_GET['page'])) {
   elseif ($page == 'user') { include "page/user/user.php"; }
   elseif ($page == 'user_tambah') { include "page/user/user_tambah.php"; }
   elseif ($page == 'user_edit') { include "page/user/user_edit.php"; }
-  
+
   // -------------------- Kategori -----------------------------
   elseif ($page == 'kategori') { include "page/kategori/kategori.php"; }
   elseif ($page == 'kategori_tambah') { include "page/kategori/kategori_tambah.php"; }
@@ -36,7 +36,9 @@ if (isset($_GET['page'])) {
   elseif ($page == 'supplier_edit') { include "page/supplier/supplier_edit.php"; }
 
   // -------------------- Role -----------------------------
-  elseif ($page == 'role') { include "page/role.php"; }
+  elseif ($page == 'role') { include "page/role/role.php"; }
+  elseif ($page == 'role_tambah') { include "page/role/role_tambah.php"; }
+  elseif ($page == 'role_edit') { include "page/role/role_edit.php"; }
 
 } else {
 
